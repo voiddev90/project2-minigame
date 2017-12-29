c22f621c74c8b2197400726a29d952e3bbe29ac6
Ryan Graute <rrgraute@gmail.com>
2017-12-29 15:03:53 +0100

    spawn Acorns

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
-import Player
-import Acorn
+from Player import Player
+from Acorn import Acorn
+from random import randint
 
 
 pygame.init()
@@ -13,20 +14,49 @@ black = 255,255,255
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 screenRect = screen.get_rect()
+winTime = 120000
+spawnTime = 2000
 
-Player = Player.Player((screenRect[2] / 3), (screenRect[3] // 2) +200, width)
-Acorn = Acorn.Acorn(300, screenRect[3])
+player = Player((screenRect[2] / 3), (screenRect[3] // 2) +200, width)
 
 gameActive = True
+acorns = []
+lastSpawn = 0
+
+def moveAcorns():
+    global acorns
+    global screen
+    newList = []
+    for acorn in acorns:
+        acorn.moveDown()
+        acorn.render(screen)
+        if not collided(acorn) and not acorn.offScreen():
+            newList.append(acorn)
+    acorns = newList
+
+def spawnAcorn():
+    global acorns, width, height, lastSpawn
+    randomPoint = randint(0, (width - 200))
+    if (randomPoint - lastSpawn) <= 10:
+        randomPoint += 30
+    acorns.append(Acorn(randomPoint, height))
+    lastSpawn = randomPoint
+
+def collided(acorn):
+    global player
+    if acorn.isCollidedWith(player):
+        player.decreaseLife()
+        return True
+    return False
 
 def movement():
     global gameActive
     keyDown = pygame.key.get_pressed()
     pygame.key.set_repeat(1,50)
     if keyDown[pygame.K_d or keyDown[pygame.K_RIGHT]]:
-        Player.moveRight()
+        player.moveRight()
     elif keyDown[pygame.K_a] or keyDown[pygame.K_LEFT]:
-        Player.moveLeft()
+        player.moveLeft()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -36,16 +66,18 @@ def movement():
             if event.key == pygame.K_ESCAPE:
                 gameActive = False
 
+lastSpawn = pygame.time.get_ticks()
 while gameActive:
     movement()
-    Acorn.moveDown()
     screen.fill(black)
-    Player.render(screen)
-    Acorn.render(screen)
-
-    if Acorn.isCollidedWith(Player):
-        Player.decreaseLife()
-
-    screen.blit(font.render('Lifes: ' + str(Player.lifes),  False, (0,0,0)), (0,0))
+    player.render(screen)
+    moveAcorns()
+    now = pygame.time.get_ticks()
+    if len(acorns) < 5 and (now - lastSpawn) >= spawnTime:
+        spawnAcorn()
+        lastSpawn = now
+    screen.blit(font.render('Lifes: ' + str(player.lifes),  False, (0,0,0)), (0,0))
+    if now == winTime:
+        print("Win!")
     clock.tick(30)
     pygame.display.flip()
